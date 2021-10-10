078f4e98b037b08fe37c0af0856a1fa87c73f72b
Lucas de Brito Fuck <54750263+lucasdebritofuck@users.noreply.github.com>
2021-10-09 23:13:38 -0300

    Commit inicial
    Projeto gerado no Spring Initalizr

---
+++ src/main/java/br/com/lucasfuck/vacancy/VacancyApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasfuck.vacancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyApplication.class, args);
+	}
+
+}
